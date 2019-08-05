afbbbbf3eed6360aebc188b45873727980b04136
CategorIAN <ianholm9@gmail.com>
2019-08-05 13:49:04 -0600

    Moved RandomPerm to test folder.

--- src/test/python/test_Cycle.py
+++ src/test/python/test_Cycle.py
@@ -1,5 +1,5 @@
-from .RandomPerm import RandomPerm
 from random import random
+from src.test.python.RandomPerm import RandomPerm
 
 def mul_degree(I, d = 10):
     R = RandomPerm()
@@ -14,7 +14,7 @@ def mul_degree(I, d = 10):
         print("g * h is {}".format(m))
         assert (m.degree == g.degree or m.degree == h.degree), "degree is {}".format(m.degree)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # mul_degree(10, 10)
     assert (5 == 5), "5 == 5"
 
